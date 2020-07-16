2736471a481b6b6d2d36c7a74bd0088febd91624
Francis Ferri <francisferrir@gmail.com>
2020-07-16 00:02:47 -0500

    Introduccion
    npm install -g typescript
    tsc --version
    
    En power Shel como administrador ejecuta:
    Set-ExecutionPolicy RemoteSigned -force

---
+++ src/hola.js
@@ -0,0 +1,4 @@
+var saludo = "Bienvenidos a TypeScript";
+console.log(saludo);
+var fecha = "27/08/2019";
+console.log("Fecha", fecha);
---
+++ src/hola.ts
@@ -0,0 +1,8 @@
+let saludo = "Bienvenidos a TypeScript";
+console.log(saludo);
+
+let fecha = "27/08/2019"
+console.log("Fecha", fecha);
+
+let edad = 25;
+
