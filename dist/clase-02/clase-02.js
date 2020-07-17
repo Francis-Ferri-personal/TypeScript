303948e57c9fcb40ca41212be8e4edf50475f9aa
Francis Ferri <francisferrir@gmail.com>
2020-07-17 15:57:38 -0500

    Tipos de datos basicos

---
+++ dist/clase-02/clase-02.js
@@ -0,0 +1,47 @@
+"use strict";
+// Tipos primitivos o tipos basicos
+// Boolena
+var estadoConectado = true;
+var estaInscrito;
+estaInscrito = false;
+estaInscrito = true;
+//estaInscrito = 2; No es valido
+function tieneDescuento(curso) {
+    if (curso === "typescript")
+        return true;
+    return false;
+}
+console.log("tieneDescuento", tieneDescuento("angular"));
+// Number
+var estudiantes = 100;
+// estudiantes = "100"; // Invalido
+var cantidadEstudiantes;
+cantidadEstudiantes = 120;
+function getCantidadEstudiantes(curso) {
+    if (curso === "typescript")
+        return 100;
+    return 0;
+}
+var inscritosCursoTypeXcript = 100;
+// Type: Number, hexadecimales, binarios, octal
+var decimal = 10;
+var hexadecimal = 0xf00d;
+var binario = 10;
+var octal = 493; // Solo numeros comprendidos entre 0 y 7
+console.log("hexadecimal: ", hexadecimal);
+console.log("decimal: ", decimal);
+console.log("binario: ", binario);
+console.log("octal: ", octal);
+// String
+var nombre = "Francis";
+var apellido = "Ferri";
+var nombtreCompleto = nombre + " " + apellido;
+console.log("Nombre completo: ", nombtreCompleto);
+// STring + ES6 = Tempalte literal
+var nombtreCompleto2 = nombre + " " + apellido;
+console.log("Nombre completo 2: ", nombtreCompleto2);
+var arregloCursos = ["TypeScript", "Angular"];
+var mensaje = "\nMi niombre completo es " + nombtreCompleto + " y tengo " + arregloCursos.length + " cursos.\n\u00A1Gracias por asistir!\n";
+var mensaje2 = "\n Mi nombre completo es " + nombtreCompleto + ' y tengo ' + arregloCursos.length + ' cursos.\n¡Gracias por asistir!';
+console.log("Mensaje", mensaje);
+console.log("Mensaje2", mensaje2);
---
+++ src/clase-02/clase-02.ts
@@ -0,0 +1,64 @@
+// Tipos primitivos o tipos basicos
+// Boolena
+let estadoConectado = true;
+let estaInscrito: boolean;
+
+estaInscrito = false;
+estaInscrito = true;
+//estaInscrito = 2; No es valido
+
+function tieneDescuento(curso: string): boolean {
+    if(curso === "typescript")
+        return true;
+    return false;
+} 
+console.log("tieneDescuento", tieneDescuento("angular"));
+
+
+// Number
+let estudiantes = 100;
+// estudiantes = "100"; // Invalido
+let cantidadEstudiantes: number
+cantidadEstudiantes = 120;
+
+
+function getCantidadEstudiantes(curso: string): number{
+    if(curso === "typescript")
+        return 100;
+    return 0;
+}
+
+let inscritosCursoTypeXcript: number = 100;
+
+
+// Type: Number, hexadecimales, binarios, octal
+let decimal: number = 10;
+let hexadecimal: number = 0xf00d;
+let binario: number = 0b1010;
+let octal: number = 0o755// Solo numeros comprendidos entre 0 y 7
+console.log("hexadecimal: ", hexadecimal);
+console.log("decimal: ", decimal);
+console.log("binario: ", binario);
+console.log("octal: ", octal);
+
+// String
+let nombre = "Francis";
+let apellido: string = "Ferri";
+let nombtreCompleto = nombre + " " + apellido;
+console.log("Nombre completo: ", nombtreCompleto);
+
+
+// STring + ES6 = Tempalte literal
+let nombtreCompleto2: string = `${nombre} ${apellido}`;
+console.log("Nombre completo 2: ", nombtreCompleto2);
+let arregloCursos = ["TypeScript", "Angular"];
+let mensaje: string = `
+Mi niombre completo es ${nombtreCompleto} y tengo ${arregloCursos.length} cursos.
+¡Gracias por asistir!
+`;
+
+let mensaje2: string = "\n Mi nombre completo es "+ nombtreCompleto + ' y tengo ' + arregloCursos.length + ' cursos.\n¡Gracias por asistir!'
+console.log("Mensaje", mensaje);
+console.log("Mensaje2", mensaje2);
+
+
