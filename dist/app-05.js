48db9c17425a5364f17a1df7cbc4e62e11a768d5
Francis Ferri <francisferrir@gmail.com>
2020-08-02 20:31:42 -0500

    Atributos de solo lectura

---
+++ dist/app-05.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// METODOS SET Y GET
+// ATRIBUTOS DE SOLO LECTURA: raedonly
+// <<< EN EXTREMOP UTIL
+var Curso = /** @class */ (function () {
+    function Curso(_id, _nombre) {
+        this._id = _id;
+        this._nombre = _nombre;
+    }
+    Object.defineProperty(Curso.prototype, "id", {
+        get: function () {
+            // Permite obtener el estado de "id"
+            return this._id;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Curso.prototype, "nombre", {
+        /* set id(id: number){
+            this._id = id;
+        } */
+        get: function () {
+            return this._nombre;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Curso;
+}());
+// >>>
+var EscuelaDigital = /** @class */ (function () {
+    // Atributos
+    // Funciones
+    function EscuelaDigital(_nombre, cursos) {
+        if (cursos === void 0) { cursos = []; }
+        this._nombre = _nombre;
+        this.cursos = cursos;
+    }
+    Object.defineProperty(EscuelaDigital.prototype, "nombre", {
+        get: function () {
+            return this._nombre;
+        },
+        set: function (nombre) {
+            this._nombre = nombre;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    EscuelaDigital.prototype.agregarCurso = function (curso) {
+        this.cursos.push(curso);
+    };
+    return EscuelaDigital;
+}());
+var escuela = new EscuelaDigital("EDteam");
+var cursoJS = new Curso(1, "JavaScript");
+var cursoTS = new Curso(2, "TypeSript");
+escuela.agregarCurso(cursoJS);
+escuela.agregarCurso(cursoTS);
+// Acceder a los atributos publicos
+// MODO LECTURA
+//cursoJS.nombre = "JavaScript desde cero"; // Esto funciona como el SET
+console.log("nombre cursoJS", cursoJS.nombre); // Leyendo estado Get
+//cursoJS.id = 20; // Aisgnacion SET
+console.log("escuela", escuela);
+console.log("Nombre Escuela: ", escuela.nombre);
---
+++ src/clase-04/app-05.ts
@@ -0,0 +1,59 @@
+export {}; // Para evitar problemas de reuso
+// METODOS SET Y GET
+// ATRIBUTOS DE SOLO LECTURA: raedonly
+
+
+// <<< EN EXTREMOP UTIL
+class Curso {
+    constructor(private readonly _id: number, private readonly _nombre: string){}
+
+    get id(){
+        // Permite obtener el estado de "id"
+        return this._id;
+    }
+
+    /* set id(id: number){
+        this._id = id;
+    } */
+    get nombre(){
+        return this._nombre
+    }
+    /* set nombre(nombre: string){
+        this._nombre = nombre;
+    } */
+}
+
+// >>>
+
+class EscuelaDigital{
+    // Atributos
+
+    // Funciones
+    constructor(private _nombre: string, private cursos: Curso[] = []){}
+
+    set nombre(nombre: string){
+        this._nombre = nombre;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    agregarCurso(curso: Curso){
+        this.cursos.push(curso);
+    }
+} 
+
+const escuela = new EscuelaDigital("EDteam");
+let cursoJS = new Curso(1, "JavaScript");
+let cursoTS = new Curso(2, "TypeSript");
+escuela.agregarCurso(cursoJS);
+escuela.agregarCurso(cursoTS);
+// Acceder a los atributos publicos
+// MODO LECTURA
+//cursoJS.nombre = "JavaScript desde cero"; // Esto funciona como el SET
+console.log("nombre cursoJS", cursoJS.nombre);// Leyendo estado Get
+
+
+//cursoJS.id = 20; // Aisgnacion SET
+console.log("escuela", escuela);
+console.log("Nombre Escuela: ",escuela.nombre);
+
