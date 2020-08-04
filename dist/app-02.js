f415698814ac7eaf8cddb99da3f1b05e1e3bf1dc
Francis Ferri <francisferrir@gmail.com>
2020-08-03 20:44:41 -0500

    Librerias para TypeScript
    http://definitelytyped.org/

--- dist/app-02.js
+++ dist/app-02.js
@@ -1,9 +1,10 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Usar unalibreria externa JavaScript
 // package.json
 // npm init -y
 // npm install lodash --save 
+Object.defineProperty(exports, "__esModule", { value: true });
+// npm i @types/lodash --save-dev
 var lodash_1 = require("lodash");
 function numeroAleatorio(a, b) {
     return lodash_1.random(a, b);
@@ -11,3 +12,4 @@ function numeroAleatorio(a, b) {
 console.log("Numero aleatorio 1 - 10", numeroAleatorio(1, 10));
 // ya no se necesita envolverla dentro de una funcion
 console.log("random", lodash_1.random(1, 10));
+console.log("random", lodash_1.random(50, 80));
--- src/clase-05/app-02.ts
+++ src/clase-05/app-02.ts
@@ -2,6 +2,9 @@
 // package.json
 // npm init -y
 // npm install lodash --save 
+
+// npm i @types/lodash --save-dev
+
 import {random} from "lodash";
 
 function numeroAleatorio(a: number, b: number){
@@ -11,3 +14,4 @@ console.log("Numero aleatorio 1 - 10", numeroAleatorio(1,10));
 
 // ya no se necesita envolverla dentro de una funcion
 console.log("random", random(1,10));
+console.log("random", random(50,80));
